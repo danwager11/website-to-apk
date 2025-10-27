31544a6355e3ee748c0b7065635e2e02462f5526
danwager11 <danwager11@gmail.com>
2025-10-27 03:03:07 -0400

    Update MainActivity.java

--- app/src/main/java/com/calibre/webtoapk/MainActivity.java
+++ app/src/main/java/com/calibre/webtoapk/MainActivity.java
@@ -78,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
     private ValueCallback<Uri[]> mFilePathCallback;       // Image upload
     private ActivityResultLauncher<Intent> fileChooserLauncher; // Image upload
 
-    String mainURL = "http://100.76.14.41:8083";
+    String mainURL = "http://100.76.14.41:5050";
     boolean requireDoubleBackToExit = true;
     boolean allowSubdomains = true;
 
